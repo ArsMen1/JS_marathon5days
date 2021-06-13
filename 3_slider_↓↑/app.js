3226f860c1af696284d73ca94448676e84dfdc93
ArsMen1 <71522524+ArsMen1@users.noreply.github.com>
2021-06-13 12:53:29 +0300

    Вроде сделял

--- 3_slider_↓↑/app.js
+++ 3_slider_↓↑/app.js
@@ -1,5 +1,9 @@
-const downBtn = document.querySelector(".down-button");
-const upBtn = document.querySelector(".up-button");
+const homeBtn = document.querySelector(".Home");
+const oneBtn = document.querySelector(".one");
+const twoBtn = document.querySelector(".two");
+const threeBtn = document.querySelector(".three");
+const fourBtn = document.querySelector(".four");
+const completeBtn = document.querySelector(".complete");
 const sidebar = document.querySelector(".sidebar");
 const mainSlide = document.querySelector(".main-slide");
 const container = document.querySelector(".container");
@@ -9,18 +13,25 @@ let activeSlideIndex = 0;
 
 sidebar.style.top = `-${(slideCount - 1) * 100}vh`;
 
-downBtn.addEventListener("click", () => slideChange("up"));
-
-upBtn.addEventListener("click", () => slideChange("down"));
+homeBtn.addEventListener("click", () => slideChange("home"));
+oneBtn.addEventListener("click", () => slideChange("1"));
+twoBtn.addEventListener("click", () => slideChange("2"));
+threeBtn.addEventListener("click", () => slideChange("3"));
+fourBtn.addEventListener("click", () => slideChange("4"));
+completeBtn.addEventListener("click", () => slideChange("complete"));
 
 function slideChange(direction) {
-  if (direction === "up") {
-    activeSlideIndex++;
-    if (activeSlideIndex === slideCount) activeSlideIndex = 0;
-  } else if (direction === "down") {
-    activeSlideIndex--;
-    if (activeSlideIndex < 0) activeSlideIndex = slideCount - 1;
-  }
+  direction === "home"
+    ? (activeSlideIndex = 0)
+    : direction === "1"
+    ? (activeSlideIndex = 1)
+    : direction === "2"
+    ? (activeSlideIndex = 2)
+    : direction === "3"
+    ? (activeSlideIndex = 3)
+    : direction === "4"
+    ? (activeSlideIndex = 4)
+    : (activeSlideIndex = 5);
 
   const height = container.clientHeight;
 
